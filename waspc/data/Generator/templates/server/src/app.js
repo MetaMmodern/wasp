b199b30abc4dc0063303b59c7308e965d8a0f42c
shayneczyzewski <shayne.czyzewski@gmail.com>
2022-06-07 16:10:42 -0400

    POC working, hacky

--- waspc/data/Generator/templates/server/src/app.js
+++ waspc/data/Generator/templates/server/src/app.js
@@ -19,6 +19,66 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
 
+// For Passport
+import session from 'express-session';
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: true }
+}));
+
+import passport from 'passport';
+app.use(passport.initialize());
+app.use(passport.authenticate('session'));
+
+import prisma from './dbClient.js';
+import { sign } from './core/auth.js';
+import GoogleStrategy from 'passport-google-oauth2';
+
+console.log('setting up Google');
+
+passport.use(new GoogleStrategy.Strategy({
+  clientID: process.env['GOOGLE_CLIENT_ID'],
+  clientSecret: process.env['GOOGLE_CLIENT_SECRET'],
+  callbackURL: 'http://localhost:3001/oauth2/redirect/google',
+  scope: ['email'],
+  passReqToCallback: true
+}, async function (request, accessToken, refreshToken, profile, done) {
+  let user = await prisma.user.findUnique({ where: { email: profile.email } });
+  if (!user) {
+    user = await prisma.user.create({ data: { email: profile.email, password: "password123!" } });
+  }
+
+  console.log(user.id);
+
+  return done(null, user);
+}));
+
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(user, done) {
+  done(null, user);
+});
+
+// Redirect user to Google
+app.get('/login/federated/google', passport.authenticate('google'));
+
+// Handle Google callback
+app.get('/oauth2/redirect/google',
+  passport.authenticate('google', {
+    failureRedirect: 'http://localhost:3000/login',
+    failureMessage: true
+  }),
+  async function(req, res) {
+    const user = req.session.passport.user;
+    console.log(user.id);
+    const token = await sign(user.id);
+    res.redirect('http://localhost:3000/login?token=' + token);
+});
+
 app.use('/', indexRouter)
 
 // Custom error handler.
